dcba6b3da929baf819024e2ac64d669a9e106013
Elijah2005e <115743179+Elijah2005e@users.noreply.github.com>
2022-10-18 14:26:19 -0700

    Add files via upload

---
+++ Anubis/src/Anubis.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Anubis { 
+	public static Scanner input = new Scanner(System.in);
+	public static String response;
+	public static String [][] product = new String[10][10];
+	public static void main(String[] args ) {
+		System.out.print("Welcome to the underworld! ");
+		fill();
+		print();
+	}
+	public static void fill() {
+		System.out.print("Who did you bring? ");
+		response = input.nextLine();
+		for(int i = 0; i < product.length; i++){
+			for(int j = 0; j < product[i].length; j++){
+				product[i][j] =  response;
+			}
+		}
+	}
+	public static void print(){
+		System.out.print("Here you go!");
+		for(int i = 0; i < product.length; i++){
+			for(int j = 0; j < product[i].length; j++){
+				System.out.print( product[i][j]+" " );
+			}
+			System.out.println(); 
+			}
+		}
+	}
